7ca7a1499e5a30945d6d7a3f5a9c1150fbb9433c
baDJax <mukulsharma262002@gmail.com>
2024-10-09 10:21:49 +0530

    Integrated formik and yup validation in form

---
+++ components/Form.tsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const LoginForm = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Required'),
+    password: Yup.string().required('Required'),
+  });
+
+  const onSubmit = (values:any) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <Field type="password" id="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+        </div>
+        <button type="submit">Login</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default LoginForm;
